fd2887b3280023686b4bae77c727f698d5add490
Eric Corrêa <126991412+Kiincoder@users.noreply.github.com>
2025-12-19 10:53:44 -0300

    add meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,29 @@
 import type { Metadata } from "next";
 import Link from "next/link"; 
 
+export const metadata: Metadata = {
+  title: {
+    default: "Pokédex Online | Explore o Mundo Pokémon",
+    template: "%s | Pokédex Online"
+  },
+  description: "Encontre estatísticas, tipos e habilidades de todos os seus Pokémon favoritos nesta Pokédex completa construída com Next.js.",
+  keywords: ["Pokémon", "Pokédex", "PokeAPI", "Estatísticas Pokémon", "Lista de Pokémon"],
+  authors: [{ name: "Eric" }],
+  robots: "index, follow",
+  openGraph: {
+    type: "website",
+    locale: "pt_BR",
+    siteName: "Pokédex Online",
+    images: [
+      {
+        url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png",
+        width: 50,
+        height: 50,
+        alt: "Pokédex Ícone",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
